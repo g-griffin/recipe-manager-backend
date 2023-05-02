0dba061903660fdb20d8c709cbd3f793cf2296cd
g-griffin <gretchen.griffin@gmx.de>
2023-05-02 14:46:06 +0200

    Initial commit

---
+++ src/main/java/de/g_griffin/recipe_manager_backend/RecipeManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package de.g_griffin.recipe_manager_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeManagerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/g_griffin/recipe_manager_backend/RecipeManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.g_griffin.recipe_manager_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
