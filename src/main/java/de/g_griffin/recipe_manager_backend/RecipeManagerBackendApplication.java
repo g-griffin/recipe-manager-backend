d01e1e0e435163c0ceab328b56a66be4a77ef9d6
g-griffin <gretchen.griffin@gmx.de>
2023-05-02 19:11:52 +0200

    Update .gitignore

--- src/main/java/de/g_griffin/recipe_manager_backend/RecipeManagerBackendApplication.java
+++ src/main/java/de/g_griffin/recipe_manager_backend/RecipeManagerBackendApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecipeManagerBackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RecipeManagerBackendApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(RecipeManagerBackendApplication.class, args);
+  }
 }
