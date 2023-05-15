cbd4bd68f3ade8cd9a532465f7034fbd580a1a90
g-griffin <gretchen.griffin@gmx.de>
2023-05-15 12:24:04 +0200

    Setup database: * DB configuration * Flyway migration * Refactor Entity name

--- src/main/java/de/g_griffin/recipe_manager_backend/RecipeManagerBackendApplication.java
+++ src/main/java/de/g_griffin/recipe_manager_backend/RecipeManagerBackendApplication.java
@@ -5,14 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,65 +19,50 @@ import java.util.Collection;
 @SpringBootApplication
 public class RecipeManagerBackendApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(RecipeManagerBackendApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeManagerBackendApplication.class, args);
+    }
 }
 
-interface IndexRepository extends JpaRepository<Index, Long> {}
+interface RecipeIndexRepository extends JpaRepository<RecipeIndex, Long> {
+}
 
 @RestController
-class IndexRestController {
-  @Autowired IndexRepository indexRepository;
+class RecipeIndexRestController {
+    @Autowired
+    RecipeIndexRepository recipeIndexRepository;
 
-  @GetMapping("/indices")
-  Collection<Index> all() {
-    return this.indexRepository.findAll();
-  }
+    @GetMapping("/recipeIndices")
+    Collection<RecipeIndex> all() {
+        return this.recipeIndexRepository.findAll();
+    }
 
-  @PostMapping("/indices")
-  Index newIndex(@RequestBody Index newIndex) {
-    return this.indexRepository.save(newIndex);
-  }
-}
-
-@Configuration
-class LoadDatabase {
-
-  private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
-
-  @Bean
-  CommandLineRunner initDatabase(IndexRepository repository) {
-
-    return args -> {
-      log.info(
-          "Preloading "
-              + repository.save(
-                  new Index(
-                      "Fall 2015\nPerfect Baked Potato 7\nEasiest Beef Stew 10\nShrimp Scampi 16")));
-      log.info(
-          "Preloading "
-              + repository.save(
-                  new Index(
-                      "February 2016\nFluffy Dinner Rolls 3\nRoast Chicken and Winter Vegetables 5")));
-    };
-  }
+    @PostMapping("/recipeIndices")
+    RecipeIndex newRecipeIndex(@RequestBody RecipeIndex newRecipeIndex) {
+        return this.recipeIndexRepository.save(newRecipeIndex);
+    }
 }
 
 @Entity
-class Index {
+class RecipeIndex {
 
-  @Id @GeneratedValue @Getter private Long id;
-  @Getter @Setter private String indexText;
+    @Id
+    @GeneratedValue
+    @Getter
+    private Long id;
+    @Getter
+    @Setter
+    private String recipeIndexText;
 
-  public Index() {}
+    public RecipeIndex() {
+    }
 
-  public Index(String indexText) {
-    this.indexText = indexText;
-  }
+    public RecipeIndex(String recipeIndexText) {
+        this.recipeIndexText = recipeIndexText;
+    }
 
-  @Override
-  public String toString() {
-    return "Index{" + "id=" + id + ", indexText='" + indexText + '\'' + '}';
-  }
+    @Override
+    public String toString() {
+        return "RecipeIndex{" + "id=" + id + ", indexText='" + recipeIndexText + '\'' + '}';
+    }
 }
